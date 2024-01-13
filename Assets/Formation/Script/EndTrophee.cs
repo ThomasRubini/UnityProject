c8c1cdbbd28bfc861f086d38d03bad8357d874c9
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2024-01-13 19:10:29 +0100

    Add trophee and end canvas + Create GameManager to manage global game aspects + make start canvas use GameManager

---
+++ Assets/Formation/Script/EndTrophee.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:87d75be803a5c8f0fc6c39336efe8d3f5332eec1926e1389982729c93f113067
+size 380
---
+++ Assets/Formation/Script/GameManager.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:07dc6e5d118cec07954a13fe249fb6220d39fb02cb689cc347ea788220224aa5
+size 337
