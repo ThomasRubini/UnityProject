176bec090b5494c00c2f81af7bc989c4176edcaa
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2024-01-13 19:46:43 +0100

    Show elapsed time when game is finished

---
+++ Assets/Formation/Script/EndCanvas.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fe0b8869dc70992339b2ff60bd36cc30cfd12c027c6d07135333c502f0669d83
+size 374
--- Assets/Formation/Script/GameManager.cs
+++ Assets/Formation/Script/GameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07dc6e5d118cec07954a13fe249fb6220d39fb02cb689cc347ea788220224aa5
-size 337
+oid sha256:0ad5c3a2c59aa18fef9ee9087d42123563a5a0fd7301e5a8cc2eb90071d9022b
+size 1116
