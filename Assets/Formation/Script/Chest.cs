7e5276e227f9bd04eabb48063af9529739f88580
Thomas Rubini <74205383+ThomasRubini@users.noreply.github.com>
2024-01-15 23:26:33 +0100

    show pickup animation when we pickup things

--- Assets/Formation/Script/Chest.cs
+++ Assets/Formation/Script/Chest.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43eb84b948ca1cd76493efe1fd6cb63e12a10e4a22c2afd554184b021911b78a
-size 720
+oid sha256:340dd039cf40efb6a91d8048b7d640377b43ca4ee4215172d8e11faf32d85055
+size 857
--- Assets/Formation/Script/Pickupable.cs
+++ Assets/Formation/Script/Pickupable.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:853c267ef8bddd4078bce59f8842fa8c4c30e4894881da360fe37df33cd59e04
-size 345
+oid sha256:9e62f9875c7c37c49ed1e181a2344a3ed7e822747d54f42127dcc9507474110d
+size 461
