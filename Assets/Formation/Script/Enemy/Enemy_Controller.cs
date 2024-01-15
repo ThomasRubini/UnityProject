d907b4ce241076c3c015c0a7fdf7cb9a69b638d2
melanie-hugues <melanie.hugues@etu.univ-amu.fr>
2024-01-16 00:35:55 +0100

    Correction Bug 2 for early access

--- Assets/Formation/Script/Enemy/Enemy_Controller.cs
+++ Assets/Formation/Script/Enemy/Enemy_Controller.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:364be690d840dfa88abfbc813de2eba5d5b3ca485742912fad763c41356fa95a
-size 4734
+oid sha256:80e0c2409f8e3805d9b7b49517a2322af8449ddacd6f5e766e85d8495221d444
+size 4688
